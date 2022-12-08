d95ec38082b346d18cdd8f20c0bf717e6507d923
TichaTin <ticha.tin@gmail.com>
2022-12-08 14:15:41 +1100

    Brand section built

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {google, slack, atlassian, dropbox, shopify } from './import.js'
 import './brand.css'
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='gpt3__brand section__padding'>
+      <div className='gpt3__brand__wrapper'>
+        <img src={google} alt="google" />
+        <img src={slack} alt="slack" />
+        <img src={atlassian} alt="atlassian" />
+        <img src={dropbox} alt="dropbox" />
+        <img src={shopify} alt="shopify" />
+      </div>
     </div>
   )
 }
