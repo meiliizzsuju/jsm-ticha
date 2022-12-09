3e7e3d85de19d7f87fdc2e67134762bfbd800cbf
TichaTin <ticha.tin@gmail.com>
2022-12-09 15:01:22 +1100

    CTA section built

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,9 +3,15 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
-    </div>
+    <section className='gpt3__cta section__margin'>
+      <div className='gpt3__cta-container'>
+        <div className='gpt3__cta-content'>
+          <span>Request Early Access to Get Started</span>
+          <h2>Register today & start exploring the endless possiblities.</h2>
+        </div>
+        <button type='button' className='gpt3__cta-button'>Get Started</button>
+      </div>
+    </section>
   )
 }
 
