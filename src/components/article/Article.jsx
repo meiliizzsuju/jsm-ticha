a8a664722fdd06ae039177f0e4f041dd2e78bf76
TichaTin <ticha.tin@gmail.com>
2022-12-10 07:48:59 +1100

    Blog section built

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './article.css'
 
-const Article = () => {
+const Article = ({img_path,title,featured,datetime}) => {
+  const date = new Date(datetime);
+  const dateString = date.toDateString('en-AU').split(' ')
+  const formatedDate = dateString[1]+' '+dateString[2]+', '+dateString[3]
+
   return (
-    <div>
-      Article
-    </div>
+    <article className={'gpt3__article '+(featured === true ?'featured': '')}>
+      <div className='gpt3__article-image'>
+        <img src={require(`../../assets/${img_path}`)} alt={title+" image"} />
+      </div>
+      <div className='gpt3__article-content'>
+        <div className='gpt3__article-content__top'>
+          <time dateTime={datetime}>{formatedDate}</time>
+          <h3>{title}</h3>
+        </div>
+        <footer>
+          <a href='#temp'>Read Full Article</a>
+        </footer>
+      </div>
+    </article>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Article } from '../../components';
+import jsonFile from './blogData.json';
+import './blog.css'
+
+const articleData = jsonFile.blogDate;
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className='gpt3__blog section__margin'>
+      <div className='gpt3__blog-title'>
+        <h2 className='gradient__text'>A lot is happening, <br/>We are blogging about it.</h2>
+      </div>
+      <div className='gpt3__blog-aticles'>
+        {
+          articleData.map((article,index)=>(
+            index <= 4
+            ? (<Article 
+                img_path={article.img}
+                title={article.title}
+                featured={article.featured}
+                datetime={article.datetime}
+                key={index}
+              />)
+            : null
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
