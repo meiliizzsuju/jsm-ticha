0166d6f207b40f652ab44e0ed274e0e99599e7e4
TichaTin <ticha.tin@gmail.com>
2022-12-10 07:50:22 +1100

    error fixes

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -9,7 +9,7 @@ const Possibility = () => {
     <section className={gmt3_possibility+" section__margin"}>
       <div className={gmt3_possibility+'__content'}>
         <span>Request Early Access to Get Started</span>
-        <h2 class="gradient__text">The possibilities are <br className='large'/>beyond your imagination</h2>
+        <h2 className="gradient__text">The possibilities are <br className='large'/>beyond your imagination</h2>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <p><a href="#">Request Early Access to Get Started</a></p>
       </div>
